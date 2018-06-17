9696aa618d9d333c4ab51d131e678bd5809aad37
Benoit Lagae <benoit.lagae@itextpdf.com>
2018-06-17 18:08:06 +0200

    Add tests

---
+++ open_words/tests/Test_Crash.py
@@ -0,0 +1,45 @@
+from open_words.parse import Parse
+
+import unittest
+import json
+
+
+class CrashTest(unittest.TestCase):
+
+    def __init__(self, meth):
+        super().__init__(meth)
+        self.par = Parse()
+
+    def parse(self, word):
+        print(json.dumps(self.par.parse(word), indent=2))
+
+    def test_unique(self):
+        self.parse("quodcumque")
+
+    def test_esse(self):
+        self.parse("sum")
+
+    def test_regular(self):
+        self.parse("cecidit")
+
+    def test_immutable(self):
+        self.parse("et")
+
+    def test_vol(self):
+        self.parse("vult")
+
+    def test_optional_enclitic(self):
+        self.parse("pollice")
+
+    def test_pronoun(self):
+        self.parse("se")
+
+    def test_preposition(self):
+        self.parse("super")
+
+    def test_personal_pronoun(self):
+        self.parse("tu")
+
+    def test_repeat(self):
+        self.parse("habes")
+        self.parse("habes")
