4e0ce0d49e27b054fbf644ff6ee833e508d2cb3c
Benoit Lagae <benoit.lagae@itextpdf.com>
2018-06-24 12:40:56 +0200

    Add minimal test with actual result

--- open_words/tests/unit/Test_Api.py
+++ open_words/tests/unit/Test_Api.py
@@ -6,7 +6,7 @@ import unittest
 class MinimalTest(unittest.TestCase):
     @staticmethod
     def make_minimal_parse():
-        return Parser(wordlist=[], addons=dict(), stems=[], uniques=[], inflects=dict(), wordkeys=dict())
+        return Parser(wordlist=[], addons=dict(), stems=dict(), uniques=dict(), inflects=dict(), wordkeys=dict())
 
     def test_empty(self):
         prs = MinimalTest.make_minimal_parse()
@@ -28,4 +28,17 @@ class MinimalTest(unittest.TestCase):
         prs.inflects["0"] = {'': [{'ending': ''}]}
         result = prs.parse("word")
         expected = {'word': 'word', 'defs': []}
+        self.assertEqual(result, expected)
+
+    def test_minimal_with_result(self):
+        prs = MinimalTest.make_minimal_parse()
+        word = {'pos': 'NUM', 'n': 'a', 'form': 'abc', 'wid': '0', 'parts': ['word'], 'senses': [], 'orth': 'word'}
+        prs.wordkeys["word"] = word
+        prs.inflects["0"] = {'': [{'ending': '', 'pos': 'NUM', 'n': 'a', 'form': ''}]}
+        prs.stems["word"] = [word]
+        prs.wordlist.append(word)
+        result = prs.parse("word")
+        expected = {'word': 'word',
+                    'defs': [{'orth': ['word'], 'senses': [],
+                              'infls': [{'stem': 'word', 'ending': '', 'pos': 'number', 'form': {'form': ['']}}]}]}
         self.assertEqual(result, expected)
